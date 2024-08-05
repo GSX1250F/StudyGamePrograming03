0d686b09ba39e5cd4ae34a63135db7a8e3aed1cc
tanada@sandagakuen.ed.jp <tanada@sandagakuen.ed.jp>
2024-08-05 22:55:13 +0900

    back ground

--- StudyGamePrograming03/BackGround.cpp
+++ StudyGamePrograming03/BackGround.cpp
@@ -7,37 +7,37 @@
 BackGround::BackGround(Game* game) :Actor(game)
 {
 	//1‚Â–Ú‚Ì”wŒi
-	Actor* bgactor = new Actor(GetGame());
-	bgactor->SetPosition(Vector2(0.0f, GetGame()->mWindowHeight * 0.5f));
+	Actor* bgactor = new Actor(game);
+	bgactor->SetPosition(Vector2(0.0f, game->mWindowHeight * 0.5f));
 	SpriteComponent* sc = new SpriteComponent(bgactor, 5);
-	sc->SetTexture(GetGame()->GetRenderer()->GetTexture("Assets/Farback01.png"));
+	sc->SetTexture(game->GetRenderer()->GetTexture("Assets/Farback01.png"));
 	MoveComponent* mc = new MoveComponent(bgactor);
 	mc->SetVelocity(Vector2(-10.0f, 0.0f));
 	mBGs.emplace_back(bgactor);
 
 	//2‚Â–Ú‚Ì”wŒi
-	bgactor = new Actor(GetGame());
-	bgactor->SetPosition(Vector2(GetGame()->mWindowWidth, GetGame()->mWindowHeight * 0.5f));
+	bgactor = new Actor(game);
+	bgactor->SetPosition(Vector2(game->mWindowWidth, game->mWindowHeight * 0.5f));
 	sc = new SpriteComponent(bgactor, 5);
-	sc->SetTexture(GetGame()->GetRenderer()->GetTexture("Assets/Farback02.png"));
+	sc->SetTexture(game->GetRenderer()->GetTexture("Assets/Farback02.png"));
 	mc = new MoveComponent(bgactor);
 	mc->SetVelocity(Vector2(-10.0f, 0.0f));
 	mBGs.emplace_back(bgactor);
 
 	//3‚Â–Ú‚Ì”wŒi
-	bgactor = new Actor(GetGame());
-	bgactor->SetPosition(Vector2(0.0f, GetGame()->mWindowHeight * 0.5f));
+	bgactor = new Actor(game);
+	bgactor->SetPosition(Vector2(0.0f, game->mWindowHeight * 0.5f));
 	sc = new SpriteComponent(bgactor, 10);
-	sc->SetTexture(GetGame()->GetRenderer()->GetTexture("Assets/Stars.png"));
+	sc->SetTexture(game->GetRenderer()->GetTexture("Assets/Stars.png"));
 	mc = new MoveComponent(bgactor);
 	mc->SetVelocity(Vector2(-20.0f, 0.0f));
 	mBGs.emplace_back(bgactor);
 
 	//4‚Â–Ú‚Ì”wŒi
-	bgactor = new Actor(GetGame());
-	bgactor->SetPosition(Vector2(GetGame()->mWindowWidth, GetGame()->mWindowHeight * 0.5f));
+	bgactor = new Actor(game);
+	bgactor->SetPosition(Vector2(game->mWindowWidth, game->mWindowHeight * 0.5f));
 	sc = new SpriteComponent(bgactor, 10);
-	sc->SetTexture(GetGame()->GetRenderer()->GetTexture("Assets/Stars.png"));
+	sc->SetTexture(game->GetRenderer()->GetTexture("Assets/Stars.png"));
 	mc = new MoveComponent(bgactor);
 	mc->SetVelocity(Vector2(-20.0f, 0.0f));
 	mBGs.emplace_back(bgactor);
